0c091aedd9fc39dd5e066d93c561e3a704960b35
Adesida Leye <adeleyeadesida@gmail.com>
2023-01-30 12:07:57 -0800

    updated script files

--- public/script.js
+++ public/script.js
@@ -5,9 +5,7 @@ const shortendLinks = document.querySelector('.shortened-links');
 shortenLinkBtn.addEventListener('click', shortenLink);
 
 function shortenLink(){
-    fetch('https://api.shrtco.de/v2/').then(res => res.json()).then(shortLink =>{
-        console.log(shortLink);
-    }).catch(error =>{
-        console.log(error);
-    })
+    if (linkInput.value == '') {
+        console.log('Empty');
+    }
 }
