32418af1d220ff86a4151e3f4af535333789309c
Adesida Leye <adeleyeadesida@gmail.com>
2023-01-31 06:47:20 -0800

    added error msg in js

--- public/script.js
+++ public/script.js
@@ -1,11 +1,19 @@
 const linkInput = document.querySelector('.link-input');
 const shortenLinkBtn = document.querySelector('.link-btn');
+const linkDiv = document.querySelector('.link-div');
 const shortendLinks = document.querySelector('.shortened-links');
+
+//Creating Error Msg
+const span = document.createElement('span')
+const errorMsg = document.createTextNode('Please add a link')
+span.appendChild(errorMsg)
 
 shortenLinkBtn.addEventListener('click', shortenLink);
 
 function shortenLink(){
     if (linkInput.value == '') {
-        console.log('Empty');
+        linkInput.classList.remove('border-none');
+        linkInput.classList.add('border-solid');
+        linkDiv.appendChild(span)
     }
 }
