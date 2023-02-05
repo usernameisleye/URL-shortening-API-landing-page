4f6cd6e8c6dbea5aafbea8569bff119aaf3b455a
Adesida Leye <adeleyeadesida@gmail.com>
2023-02-05 15:22:27 +0100

    written most shortening js code

--- public/script.js
+++ public/script.js
@@ -1,19 +1,31 @@
 const linkInput = document.querySelector('.link-input');
 const shortenLinkBtn = document.querySelector('.link-btn');
-const linkDiv = document.querySelector('.link-div');
 const shortendLinks = document.querySelector('.shortened-links');
-
-//Creating Error Msg
-const span = document.createElement('span')
-const errorMsg = document.createTextNode('Please add a link')
-span.appendChild(errorMsg)
+const errorMsg = document.querySelector('.link-div span');
+const originalLink = document.querySelector('.original-link');
+const shortLink = document.querySelector('.short-link');
+const copyBtn = document.querySelector('.copy-btn');
 
 shortenLinkBtn.addEventListener('click', shortenLink);
 
 function shortenLink(){
     if (linkInput.value == '') {
-        linkInput.classList.remove('border-none');
-        linkInput.classList.add('border-solid');
-        linkDiv.appendChild(span)
+        linkInput.classList.add('border-solid', 'border-Red', 'border-2');
+        errorMsg.textContent = 'Please add a link';
+    }
+    else{
+        fetch(`https://api.shrtco.de/v2/shorten?url=${linkInput.value}`).then(res => res.json()).then(outputLink => {
+            console.log(outputLink);
+            originalLink.textContent = outputLink.result.original_link;
+            shortLink.textContent = outputLink.result.short_link;
+
+            linkInput.classList.remove('border-solid', 'border-2', 'border-Red');
+            errorMsg.textContent = '';
+        }).catch(err =>{
+            errorMsg.textContent = 'Invalid URL, Please Enter a Valid URL...';
+            linkInput.classList.add('border-solid', 'border-Red', 'border-2');
+
+            console.log(err);
+        })
     }
 }
