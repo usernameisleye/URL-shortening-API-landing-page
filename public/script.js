79cba2e8a777fe4aa0042f641e1af5669d767bbe
Adesida Leye <adeleyeadesida@gmail.com>
2023-02-05 15:38:00 +0100

    added short link div code

--- public/script.js
+++ public/script.js
@@ -7,6 +7,7 @@ const shortLink = document.querySelector('.short-link');
 const copyBtn = document.querySelector('.copy-btn');
 
 shortenLinkBtn.addEventListener('click', shortenLink);
+copyBtn.addEventListener('click', copyLink)
 
 function shortenLink(){
     if (linkInput.value == '') {
@@ -15,10 +16,21 @@ function shortenLink(){
     }
     else{
         fetch(`https://api.shrtco.de/v2/shorten?url=${linkInput.value}`).then(res => res.json()).then(outputLink => {
-            console.log(outputLink);
+            let shortLinkDiv = '';
             originalLink.textContent = outputLink.result.original_link;
             shortLink.textContent = outputLink.result.short_link;
 
+            shortLinkDiv = ` <div class="shortened-links flex flex-col gap-4 w-3/4">
+                                <div class="shortened-link bg-White flex sm:flex-row flex-col justify-between items-center p-4 rounded-md">
+                                    <span class="original-link sm:border-none border-b-2 border-Gray border-solid w-full sm:text-start text-center">${originalLink.value}</span>
+
+                                <div class="flex sm:flex-row flex-col justify-end items-center gap-2 sm:mt-0 mt-4 w-full">
+                                    <span class="text-Cyan short-link cursor-pointer">${shortLink.value}</span>
+                                    <button class="copy-btn bg-Cyan h-9 px-8 sm:w-fit w-full rounded-md font-medium text-White hover:brightness-105 outline-none">Copy</button>
+                                </div>
+                            </div>`;
+
+                          
             linkInput.classList.remove('border-solid', 'border-2', 'border-Red');
             errorMsg.textContent = '';
         }).catch(err =>{
@@ -28,4 +40,15 @@ function shortenLink(){
             console.log(err);
         })
     }
+}
+
+function copyLink(e){
+    navigator.clipboard.writeText(shortLink.textContent);
+    
+    copyBtn.textContent = 'Copied!';
+    copyBtn.style.backgroundColor = 'hsl(257, 27%, 26%)';
+    setTimeout(() => {
+        copyBtn.textContent = 'Copy';
+        copyBtn.style.backgroundColor = 'hsl(180, 66%, 49%)';
+    }, 2000)
 }
